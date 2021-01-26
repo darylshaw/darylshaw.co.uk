c087732c61fbfae68059a706a9ed6e5d80a4de12
Daryl Shaw <github@darylshaw.co.uk>
2021-01-26 10:34:32 +0000

    Updated to set path when setting cookie for ga opt in

--- assets/js/opt-in.js
+++ assets/js/opt-in.js
@@ -14,6 +14,13 @@ btnHandler('#btn-opt-in', function(){
   var now = new Date();
   var expires = now.getTime() + 1000 * 3600 * 24 * 365;
   now.setTime(expires);
-  document.cookie = 'nf_ab=ga; expires=${ now.toUTCString() }';
+  document.cookie = "nf_ab=ga; expires='+now.toUTCString()+'; path=/";
+  window.location.reload();
+})
+
+
+// Set a cookie to opt out of ga
+btnHandler('#btn-opt-out', function(){
+  document.cookie = "nf_ab=;expires=Thu, 01 Jan 1970 00:00:01 GMT;";
   window.location.reload();
 });
